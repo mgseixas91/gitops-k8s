e012aed25c1cfeafcf2351146362b90490a04d9c
mgseixas91 <mg.seixas@hotmail.com>
2025-12-04 18:41:11 -0300

    Atualiza repoURL para mgseixas91/gitops-k8s

--- gitops-envs/scripts/create_env.sh
+++ gitops-envs/scripts/create_env.sh
@@ -5,6 +5,6 @@ echo "Criando namespace $ENV"
 kubectl create ns $ENV || echo "$ENV já existe"
 
 echo "Aplicando AppSet para o ambiente $ENV"
-# Cria AppSet temporário apontando para esse ambiente
-envsubst < scripts/appset-template.yaml | kubectl apply -f -
+# Aplica o AppSet no namespace argocd
+ENV=$ENV envsubst < scripts/appset-template.yaml | kubectl apply -n argocd -f -
 
